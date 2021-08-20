21c32cee5867540f11508aa40d36b1d7a9600cf4
helmi_fauzi <helmi.fauzi@sirclo.com>
2021-08-20 19:09:58 +0700

    cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser"
 import routes from "./module/routes"
-import cors from "./middleware/cors"
+import cors from "cors"
 import errorhandler from "./middleware/errorhandler"
 
 const app = express()
@@ -10,14 +10,14 @@ require('./model')
 app.use(bodyParser.json({
     limit: "50mb"
 }))
-app.use(cors)
+app.use(cors())
 app.use(routes)
 
 // index
 app.get('/', (req, res) => {
     res.json({
         title: 'sipp',
-        body: 'Hi, hlmifzi@gmail.com'
+        body: 'Hi, rian6517@gmail.com'
     })
 })
 
