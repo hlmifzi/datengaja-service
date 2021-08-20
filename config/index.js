230214a25c548903e0d541e3f422e096c69d202b
helmi_fauzi <helmi.fauzi@sirclo.com>
2021-08-20 22:20:49 +0700

    push ssl and not

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export default {
-    port: 443,
+    port: 3443,
     secretKey: 'sAw3d@Qqqq',
     payLoad: {
         iss: 'http://localhost/',
--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-// import http from "http"
-import https from "https"
+import http from "http"
 import config from "./config"
 import app from "./app"
-import fs from "fs"
-import path from "path"
-
 
 // server listen
-const sslServer = https.createServer({
-    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-}, app)
-
-sslServer.listen(config.port, () => console.log(`secure server on ${config.port}`))
+http.createServer(app).listen(config.port, () => {
+    console.log(`Server started on port ${config.port}`)
+})
