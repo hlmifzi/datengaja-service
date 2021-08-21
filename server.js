f7ac61f405cc0a31af7b152482ff7a5788875af5
helmi_fauzi <helmi.fauzi@sirclo.com>
2021-08-21 13:28:01 +0700

    tes

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+// import http from "http"
 import http from "http"
+import https from "https"
 import config from "./config"
 import app from "./app"
+import fs from "fs"
+import path from "path"
+
 
 // server listen
 http.createServer(app).listen(config.port, () => {
     console.log(`Server started on port ${config.port}`)
 })
+
+const sslServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+}, app)
+
+
+
+sslServer.listen(443, () => console.log(`secure server on ${config.port}`))
